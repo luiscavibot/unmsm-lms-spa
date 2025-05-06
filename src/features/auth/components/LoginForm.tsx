5318dd050b83f9868d7effb6080f2e964f001285
Hanhusaso <hans.e.huiza.n@gmail.com>
2025-05-06 00:33:17 -0500

    LMS-2 Complete login view with form validation and layout

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,5 +1,3 @@
-// src/features/auth/components/LoginForm.tsx
-
 import {
 	Box,
 	Button,
@@ -19,73 +17,78 @@ import GoogleIcon from '@/assets/icons/GoogleIcon'
 	const [showPassword, setShowPassword] = useState(false)
   
 	return (
-	  <Box sx={{ width: '100%', maxWidth: 360 }}>
-		<Typography variant="h5" fontWeight={700} gutterBottom>
-		  ¡Bienvenido/a a tu aula virtual!
-		</Typography>
-  
-		<Typography variant="body2" sx={{ mb: 3 }}>
-		  Ingresa para acceder a tus cursos y materiales en línea.
-		</Typography>
-  
-		<Stack spacing={2}>
-			<TextField
-				label="Correo electrónico"
-				fullWidth
-			/>
-	
-			<TextField
-				label="Contraseña"
-				type={showPassword ? 'text' : 'password'}
-				fullWidth
-				InputProps={{
-					endAdornment: (
-						<InputAdornment position="end">
-						<IconButton
-							onClick={() => setShowPassword(!showPassword)}
-							edge="end"
-						>
-							{showPassword ? <VisibilityOff /> : <Visibility />}
-						</IconButton>
-						</InputAdornment>
-					),
-				}}
-			/>
-	
-			<Link href="#" underline="hover" fontSize="14px">
-				Olvidé mi contraseña
-			</Link>
-	
-			<Button variant="contained" fullWidth>
-				INGRESAR
-			</Button>
-	
-			<Divider>o</Divider>
-	
-			<Button
-				sx={{
-					bgcolor: 'white',
-					color: '#202124',
-					fontWeight: '600',
-					textTransform: 'initial',
-				}}
-				variant="text"
-				fullWidth
-				startIcon={
-					<GoogleIcon />
-				}
-			>
-				Inicia sesión con Google
-			</Button>
-	
-			<Typography variant="body2" align="center">
-				¿Necesitas una cuenta?{' '}
-				<Link href="#" underline="hover">
-				Contacta a soporte
-				</Link>
+		<Box width={{xs: 'auto', sm: '310px'}} maxWidth="100%">
+			<Typography sx={{ maxWidth: '183px', textAlign: 'center', marginX: 'auto', mb: '8px' }} variant="h2" fontSize={{xs: 16, md: 24}} fontWeight={800}>
+				¡Bienvenido/a <br /> a tu aula virtual!
 			</Typography>
-		</Stack>
-	  </Box>
+	
+			<Typography variant="body2" sx={{ mb: 3, textAlign: 'center' }}>
+				Ingresa para acceder a tus cursos y <br /> materiales en línea.
+			</Typography>
+	
+			<Stack spacing={2}>
+				<TextField
+					label="Correo electrónico"
+					fullWidth
+				/>
+		
+				<TextField
+					label="Contraseña"
+					type={showPassword ? 'text' : 'password'}
+					fullWidth
+					InputProps={{
+						endAdornment: (
+							<InputAdornment position="end">
+							<IconButton
+								onClick={() => setShowPassword(!showPassword)}
+								edge="end"
+							>
+								{showPassword ? <VisibilityOff /> : <Visibility />}
+							</IconButton>
+							</InputAdornment>
+						),
+					}}
+				/>
+
+				<div>
+					<Link href="#" underline="hover" fontSize="14px" sx={{ display: 'inline-block' }}>
+						Olvidé mi contraseña
+					</Link>
+				</div>
+		
+				<Button size="large" variant="contained" fullWidth>
+					INGRESAR
+				</Button>
+		
+				<Divider>o</Divider>
+		
+				<Button
+					sx={{
+						bgcolor: 'white',
+						color: '#202124',
+						fontWeight: '600',
+						textTransform: 'initial',
+						height: 48,
+						borderRadius: '12px',
+						border: '1px solid #F7F7F7',
+					}}
+					variant="text"
+					fullWidth
+					startIcon={
+						<GoogleIcon />
+					}
+				>
+					Inicia sesión con Google
+				</Button>
+		
+				<Typography variant="body2" align="center">
+					¿Necesitas una cuenta?{' '}
+					<Link href="#" underline="hover">
+					Contacta a soporte
+					</Link>
+				</Typography>
+			</Stack>
+		</Box>
 	)
   }
   
--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,42 +1,59 @@
 // import React from 'react'
 import { LoginForm } from '@/features/auth/components/LoginForm'
-import { AppBar, Toolbar, Container, Box } from '@mui/material'
+import { AppBar, Toolbar, Stack, Box } from '@mui/material'
 import EscudoBiologiaIcon from '@/assets/icons/EscudoBiologiaIcon'
+import { useTheme } from '@mui/material/styles'
 
 const Login = () => {
+	const theme = useTheme()
 	return (
-		<div>
-			{/* Header */}
+		<Stack sx={{ flexDirection: 'column', minHeight: '100vh', }}>
 			<AppBar
 				position="static"
 				sx={{
 					bgcolor: 'transparent',
 					boxShadow: 'none',
+					padding: { xs: '10px 4px', md: '12px 64px' },
 				}}
 			>
 				<Toolbar sx={{
-					color: '#0B1621',
+					color: theme.palette.secondary.darkest,
 				}}>
 					<EscudoBiologiaIcon />
 				</Toolbar>
 			</AppBar>
-
-			{/* Contenido del Login */}
-			<Container maxWidth="xs" sx={{ marginTop: 4 }}>
-				<Box
+			<Stack
+				marginX="auto"
+				maxWidth="xl"
+				paddingX={{xs: '4px', sm: '87px'}}
+				direction="row"
+				spacing={8}
 				sx={{
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-					padding: 3,
-					boxShadow: 3,
-					borderRadius: 2,
+					width: '100%',
+					justifyContent: 'space-between',
+					flexWrap: 'wrap',
+					flexGrow: '1',
 				}}
-				>
+				useFlexGap
+			>
+				<Box
+					component="img"
+					src="/src/assets/images/bg-login.png"
+					alt="bg-login"
+					sx={{
+						display: { xs: 'none', md: 'block' },
+						// maxWidth: '643px',
+						maxWidth: { xs: '350px', lg: '500px', xl: '643px' },
+						width: '100%',
+						height: 'auto',
+						objectFit: 'contain',
+					}}
+				/>
+				<Stack justifyContent="center" marginX="auto">
 					<LoginForm />
-				</Box>
-			</Container>
-		</div>
+				</Stack>
+			</Stack>
+		</Stack>
 	)
 }
 
