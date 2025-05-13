16a3795dfff17317a0fdff545ac60cf70d834ae4
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2025-05-13 16:02:30 -0500

    Actualizar etiquetas de curso en el servicio de cursos para utilizar constantes de lab

--- src/services/courses/coursesSvc.ts
+++ src/services/courses/coursesSvc.ts
@@ -1,17 +1,18 @@
 import { baseApi } from '../baseApi';
 import { ICourseResp } from './types';
+import lab from '@/services/apiLabels';
 
 export const coursesApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     getCourses: build.query<ICourseResp[], void>({
       query: () => '/courses',
       providesTags: (result) =>
-        result ? [...result.map((c) => ({ type: 'Course' as const, id: c.id })), 'Course'] : ['Course'],
+        result ? [...result.map((c) => ({ type: lab.Courses as const, id: c.id })), lab.Courses] : [lab.Courses],
     }),
 
     getCourseById: build.query<ICourseResp, string>({
       query: (id) => `/courses/${id}`,
-      providesTags: (_result, _error, id) => [{ type: 'Course', id }],
+      providesTags: (_result, _error, id) => [{ type: lab.Courses, id }],
     }),
   }),
   overrideExisting: false,
