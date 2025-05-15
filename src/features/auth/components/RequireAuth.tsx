39a9528ba778736d2d73dca482160ec884065e4c
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2025-05-15 15:53:42 -0500

    Eliminar comentarios innecesarios en el componente RequireAuth para mejorar la claridad del código.

--- src/features/auth/components/RequireAuth.tsx
+++ src/features/auth/components/RequireAuth.tsx
@@ -1,4 +1,3 @@
-// src/components/RequireAuth.tsx
 import { useAppSelector } from '@/store/hooks';
 import React from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
@@ -7,7 +6,6 @@ const RequireAuth: React.FC = () => {
   const token = useAppSelector((s) => s.auth.accessToken);
   const location = useLocation();
 
-  // Si hay token, renderiza las rutas hijas; si no, fuerza login
   return token ? <Outlet /> : <Navigate to="/login" replace state={{ from: location }} />;
 };
 
