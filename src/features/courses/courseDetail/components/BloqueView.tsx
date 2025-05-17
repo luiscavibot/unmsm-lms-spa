0aedbf58af735a672bbdf1e6e90e1aacb278c0cc
Hanhusaso <hans.e.huiza.n@gmail.com>
2025-05-17 04:57:10 -0500

    LMS-17 create info bloque

--- src/features/courses/courseDetail/components/BloqueView.tsx
+++ src/features/courses/courseDetail/components/BloqueView.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import CoursesDetailTabs from './CoursesDetailTabs';
 import TabPanel from '../../components/TabPanel';
 import MaterialesView from '../views/MaterialesView';
 import AlumnoAsistenciaView from '../views/AlumnoAsistenciaView';
 import AlumnoNotasView from '../views/AlumnoNotasView';
+import { ContentPaste, Videocam } from '@mui/icons-material';
 
 interface BloqueViewProps {
   value: string;
@@ -16,12 +17,60 @@ export default function BloqueView({ value, nombre }: BloqueViewProps) {
   const handleChange = (_event: React.SyntheticEvent, newValueTab: number) => {
     setValueTab(newValueTab);
   };
+  const handleClickSyllabus = () => {
+    console.log('Ir a syllabus');
+  };
+  const handleClickCV = () => {
+    console.log('Ir a CV');
+  };
+  const handleClickClass = () => {
+    console.log('Ir a clase');
+  };
 
   return (
     <Box sx={{ px: '24px', py: '32px', bgcolor: 'neutral.lightest', borderRadius: '8px' }}>
       <Typography variant="h6" sx={{ mb: '24px', fontSize: '20px', fontWeight: '700', color: 'neutral.dark' }}>
         {nombre}
       </Typography>
+
+      <Box sx={{ mb: '32px', display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+          <Box sx={{ display: 'flex', alignItems: 'baseline', gap: { xs: '16px', md: '96px' }, justifyContent: 'space-between' }}>
+            <Box sx={{ display: 'flex', alignItems: 'baseline', gap: '8px' }}>
+              <Typography variant="body2" sx={{ fontSize: '14px', fontWeight: '600', color: 'neutral.dark' }}>
+                Horario:
+              </Typography>
+              <Box component="ul" sx={{ pl: 2, my: 0, listStyle: 'disc', color: 'neutral.main' }}>
+                <Box component="li" sx={{ fontSize: '14px' }}>
+                  Lunes: 9:00 - 11:00
+                </Box>
+                <Box component="li" sx={{ fontSize: '14px' }}>
+                  Jueves: 21:00 - 22:30
+                </Box>
+              </Box>
+            </Box>
+            <Box>
+              <Typography component="span" variant="body2" sx={{ fontSize: '14px', fontWeight: '600', color: 'neutral.dark' }}>
+                Aula:
+              </Typography>{' '}
+              <Typography component="span" variant="body2" sx={{ fontSize: '14px', fontWeight: '400', color: 'neutral.main' }}>
+                234
+              </Typography>
+            </Box>
+          </Box>
+          <Box sx={{ display: 'flex', gap: '10px' }}>
+            <Button size="small" startIcon={<ContentPaste />} variant="outlined" color="secondary" onClick={handleClickSyllabus}>
+              Syllabus
+            </Button>
+            <Button size="small" startIcon={<ContentPaste />} variant="outlined" color="secondary" onClick={handleClickCV}>
+              CV del docente
+            </Button>
+          </Box>
+        </Box>
+        <Button size="large" startIcon={<Videocam />} variant="contained" color="primary" onClick={handleClickClass}>
+          Ir a clase
+        </Button>
+      </Box>
 
       <CoursesDetailTabs value={valueTab} onChange={handleChange} />
       <TabPanel value={valueTab} index={0}>
