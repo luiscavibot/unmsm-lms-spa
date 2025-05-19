c10a8b3854a4622140b739b600f4610ac6460352
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2025-05-19 09:41:23 -0500

    refactor: mejorar la estructura y estilo del componente MainLayout

--- src/components/layouts/MainLayout/MainLayout.tsx
+++ src/components/layouts/MainLayout/MainLayout.tsx
@@ -1,25 +1,37 @@
-import { ReactNode } from 'react'
+import { ReactNode } from 'react';
 // import { Link } from 'react-router-dom'
-import { Box, Toolbar } from '@mui/material'
-import Header from './Header'
-import SideNav from './SideNav'
+import { Box, Container, Toolbar } from '@mui/material';
+import Header from './Header';
+import SideNav from './SideNav';
 
 interface MainLayoutProps {
-	children: ReactNode
+  children: ReactNode;
 }
-
+const CONTENT_MAX_WIDTH = 1200;
 export const MainLayout = ({ children }: MainLayoutProps) => {
-	return (
-		// <Stack sx={{ flexDirection: 'column', minHeight: '100vh', }}>
-		<Box sx={{ display: 'flex' }}>
-			<Header />
-				<SideNav />
-				<Box component="main" sx={{ flexGrow: 1 }}>
-					<Toolbar />
-					<Box sx={{ p: 3 }}>
-						{children}
-					</Box>
-				</Box>
-		</Box>
-	)
-}
+  return (
+    // <Stack sx={{ flexDirection: 'column', minHeight: '100vh', }}>
+    <Box sx={{ display: 'flex' }}>
+      <Header />
+      <SideNav />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          px: 2,
+          py: 3,
+          // Centrar y limitar ancho:
+          mx: 'auto',
+          width: '100%',
+          maxWidth: CONTENT_MAX_WIDTH,
+        }}
+      >
+        <Toolbar />
+
+        <Container disableGutters maxWidth={false} sx={{ p: 0 }}>
+          {children}
+        </Container>
+      </Box>
+    </Box>
+  );
+};
